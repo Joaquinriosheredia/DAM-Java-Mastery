ef02fa31d3eaddfcc395a772c9a7c17f995948b1
Joaquinriosheredia <j.r.sinbarreras@gmail.com>
2026-03-31 13:54:50 +0200

    Update TechSolution.java

--- BBDD_Acceso/TechSolution.java
+++ BBDD_Acceso/TechSolution.java
@@ -16,7 +16,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.io.Serializable;
-import java.util.List;
+import java.util.List; 
 
 /**
  * @param <T> La clase de entidad generada
